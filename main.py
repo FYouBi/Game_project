9508ed09eb80417e2bfe290a247556f501b237d9
Your Name <you@example.com>
2021-12-21 14:24:14 +0300

    add ii

--- main.py
+++ main.py
@@ -8,6 +8,9 @@ pygame.init()
 STEP_EVENT = pygame.USEREVENT + 1
 pygame.time.set_timer(STEP_EVENT, 200)
 
+HIT_EVENT = pygame.USEREVENT + 2
+pygame.time.set_timer(HIT_EVENT, 3000)
+
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
 clock = pygame.time.Clock()
 
@@ -20,6 +23,9 @@ while running:
             running = False
         if event.type == STEP_EVENT:
             player.do_step()
+        if event.type == HIT_EVENT:
+            for mob in mobs_sprite.sprites():
+                mob.can_hit = True
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_r:
                 player.hit()
@@ -36,10 +42,11 @@ while running:
     if KEY[pygame.K_s]:
         if player.rect.bottomleft[1] < HEIGHT:
             player.move_down()
+
     for mob in mobs_sprite.sprites():
         distance = ((int(player.rect.centerx) - int(mob.rect.centerx)) ** 2 +
                     (int(player.rect.centery) - int(mob.rect.centery)) ** 2) ** 0.5
-
+        print(mob.can_hit if mob.can_hit is False else '')
         if distance <= 250:
             mob.run()
 
--- mobs.py
+++ mobs.py
@@ -9,23 +9,40 @@ class Mob(pygame.sprite.Sprite):
     def __init__(self, *groups: AbstractGroup):
         super().__init__(*groups)
         self.image = pygame.image.load(f'images/mob_right.png')
-        self.mask = pygame.mask.from_surface(self.image)
         self.spawn = (random.randrange(100, WIDTH - 100), random.randrange(100, HEIGHT - 100))
         self.step_count = 1
+        self.can_hit = True
         self.rect = self.image.get_rect()
         self.rect.x, self.rect.y = self.spawn[0], self.spawn[1]
+        self.health = 3
 
     def run(self):
-        self.hero_pos = (player.rect.x, player.rect.y)
-        print(self.hero_pos[0], self.hero_pos[1])
-        if self.hero_pos[0] < self.rect.x:
-            self.rect.x -= 1
-        if self.hero_pos[0] > self.rect.x:
-            self.rect.x += 1
-        if self.hero_pos[1] < self.rect.y:
-            self.rect.y -= 1
-        if self.hero_pos[1] > self.rect.y:
-            self.rect.y += 1
+        self.hero_pos = (player.rect.centerx, player.rect.centery)
+        self.mask_enemy = pygame.mask.from_surface(self.image)
+        self.mask_hero = player.mask
+        self.offset = (player.rect.x - self.rect.x + 5, player.rect.y - self.rect.y + 5)
+        self.crash = self.mask_enemy.overlap_area(self.mask_hero, self.offset)
+        if self.can_hit:
+            if self.hero_pos[0] < self.rect.x - 5:
+                self.rect.x -= 1
+            if self.hero_pos[0] > self.rect.x - 5:
+                self.rect.x += 1
+            if self.hero_pos[1] < self.rect.y - 5:
+                self.rect.y -= 1
+            if self.hero_pos[1] > self.rect.y - 5:
+                self.rect.y += 1
+        else:
+            if self.hero_pos[0] < self.rect.x - 5:
+                self.rect.x += 1
+            if self.hero_pos[0] > self.rect.x - 5:
+                self.rect.x -= 1
+            if self.hero_pos[1] < self.rect.y - 5:
+                self.rect.y += 1
+            if self.hero_pos[1] > self.rect.y - 5:
+                self.rect.y -= 1
+        if self.crash > 0 and self.can_hit:
+            self.can_hit = False
+            player.health -= 1
 
 
 mobs_sprite = pygame.sprite.Group()
