bcdac3b0ec74617d6248e02dfd51a7d2e931868e
Your Name <you@example.com>
2021-12-22 21:41:33 +0300

    add ability

--- hero_and_mobs.py
+++ hero_and_mobs.py
@@ -41,68 +41,77 @@ class Hero(pygame.sprite.Sprite):
     def check_health(self):
         self.health -= 1
         if self.health <= 0:
-            print('as')
+            pass
+
+    def the_world(self):
+        for mob in mobs_sprite.sprites():
+            mob.freeze_func()
 
 
 class Mob(pygame.sprite.Sprite):
     def __init__(self, *groups: AbstractGroup):
         super().__init__(*groups)
         self.image = pygame.image.load(f'images/mob_right.png')
-        self.spawn = (random.randrange(100, WIDTH - 100), random.randrange(100, HEIGHT - 100))
+        self.spawn = (random.randrange(WIDTH // 2, WIDTH - 100), random.randrange(100, HEIGHT - 100))
         self.step_count = 1
         self.can_hit = True
+        self.freeze = False
         self.rect = self.image.get_rect()
         self.rect.x, self.rect.y = self.spawn[0], self.spawn[1]
         self.health = 3
 
     def run(self):
-        self.hero_pos = (player.rect.centerx, player.rect.centery)
-        self.mask_enemy = pygame.mask.from_surface(self.image)
-        self.mask_hero = player.mask
-        self.offset = (player.rect.x - self.rect.x + 5, player.rect.y - self.rect.y + 5)
-        self.crash = self.mask_enemy.overlap_area(self.mask_hero, self.offset)
+        if not self.freeze:
+            self.hero_pos = (player.rect.centerx, player.rect.centery)
+            self.mask_enemy = pygame.mask.from_surface(self.image)
+            self.mask_hero = player.mask
+            self.offset = (player.rect.x - self.rect.x + 5, player.rect.y - self.rect.y + 5)
+            self.crash = self.mask_enemy.overlap_area(self.mask_hero, self.offset)
 
-        for mob in mobs_sprite.sprites():
-            if pygame.sprite.collide_mask(self, mob) and self != mob:
-                if self.rect.x > mob.rect.x:
-                    self.rect.x += 1
-                    mob.rect.x -= 1
-                else:
+            for mob in mobs_sprite.sprites():
+                if pygame.sprite.collide_mask(self, mob) and self != mob:
+                    if self.rect.x > mob.rect.x:
+                        self.rect.x += 1
+                        mob.rect.x -= 1
+                    else:
+                        self.rect.x -= 1
+                        mob.rect.x += 1
+
+            if self.can_hit:
+                if self.hero_pos[0] < self.rect.x - 1:
                     self.rect.x -= 1
-                    mob.rect.x += 1
+                    self.image = pygame.image.load(f'images/mob_left.png')
+                if self.hero_pos[0] > self.rect.x - 1:
+                    self.rect.x += 1
+                    self.image = pygame.image.load(f'images/mob_right.png')
+                if self.hero_pos[1] < self.rect.y - 1:
+                    self.rect.y -= 1
+                if self.hero_pos[1] > self.rect.y - 1:
+                    self.rect.y += 1
 
-        if self.can_hit:
-            if self.hero_pos[0] < self.rect.x - 1:
-                self.rect.x -= 1
-                self.image = pygame.image.load(f'images/mob_left.png')
-            if self.hero_pos[0] > self.rect.x - 1:
-                self.rect.x += 1
-                self.image = pygame.image.load(f'images/mob_right.png')
-            if self.hero_pos[1] < self.rect.y - 1:
-                self.rect.y -= 1
-            if self.hero_pos[1] > self.rect.y - 1:
-                self.rect.y += 1
+            else:
+                if self.hero_pos[0] < self.rect.x - 1:
+                    self.rect.x += 1
+                    self.image = pygame.image.load(f'images/mob_right.png')
+                if self.hero_pos[0] > self.rect.x - 1:
+                    self.rect.x -= 1
+                    self.image = pygame.image.load(f'images/mob_left.png')
+                if self.hero_pos[1] < self.rect.y - 1:
+                    self.rect.y += 1
+                if self.hero_pos[1] > self.rect.y - 1:
+                    self.rect.y -= 1
 
-        else:
-            if self.hero_pos[0] < self.rect.x - 1:
-                self.rect.x += 1
-                self.image = pygame.image.load(f'images/mob_right.png')
-            if self.hero_pos[0] > self.rect.x - 1:
-                self.rect.x -= 1
-                self.image = pygame.image.load(f'images/mob_left.png')
-            if self.hero_pos[1] < self.rect.y - 1:
-                self.rect.y += 1
-            if self.hero_pos[1] > self.rect.y - 1:
-                self.rect.y -= 1
-
-        if self.crash > 0 and self.can_hit:
-            self.can_hit = False
-            player.check_health()
+            if self.crash > 0 and self.can_hit:
+                self.can_hit = False
+                player.check_health()
 
     def check_health(self):
         self.health -= 1
         if self.health <= 0:
             mobs_sprite.remove(self)
+
+    def freeze_func(self):
+        self.freeze = not self.freeze
 
 
 hero_sprite = pygame.sprite.Group()
--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import pygame
 from settings import *
 from hero_and_mobs import player, hero_sprite, mobs_sprite
+from camera import camera
 
 
 pygame.init()
@@ -12,7 +13,14 @@ HIT_EVENT = pygame.USEREVENT + 2
 pygame.time.set_timer(HIT_EVENT, 3500)
 
 UP_HEALTH_EVENT = pygame.USEREVENT + 3
-pygame.time.set_timer(UP_HEALTH_EVENT, KD)
+pygame.time.set_timer(UP_HEALTH_EVENT, 4000)
+
+ABILITY_READY = pygame.USEREVENT + 4
+pygame.time.set_timer(ABILITY_READY, 3000)
+ABILITY = False
+
+ABILITY_TIME = pygame.USEREVENT + 5
+pygame.time.set_timer(ABILITY_TIME, 0)
 
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
 clock = pygame.time.Clock()
@@ -24,18 +32,36 @@ while running:
         KEY = pygame.key.get_pressed()
         if event.type == pygame.QUIT:
             running = False
+
         if event.type == STEP_EVENT:
             player.do_step()
+
         if event.type == HIT_EVENT:
             for mob in mobs_sprite.sprites():
                 mob.can_hit = True
+
         if event.type == UP_HEALTH_EVENT:
             if player.health < 8:
                 player.health += 1
                 pygame.time.set_timer(UP_HEALTH_EVENT, KD)
+
+        if event.type == ABILITY_READY:
+            ABILITY = True
+            pygame.time.set_timer(ABILITY_READY, 0)
+            print('ready')
+
+        if event.type == ABILITY_TIME:
+            pygame.time.set_timer(ABILITY_TIME, 0)
+            pygame.time.set_timer(ABILITY_READY, 3000)
+            player.the_world()
+
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_r:
                 player.hit()
+            if event.key == pygame.K_x and ABILITY:
+                ABILITY = False
+                player.the_world()
+                pygame.time.set_timer(ABILITY_TIME, 3000)
 
     if KEY[pygame.K_d]:
         if player.rect.topright[0] < WIDTH:
@@ -53,7 +79,7 @@ while running:
     for mob in mobs_sprite.sprites():
         distance = ((int(player.rect.centerx) - int(mob.rect.centerx)) ** 2 +
                     (int(player.rect.centery) - int(mob.rect.centery)) ** 2) ** 0.5
-        if distance <= 400:
+        if distance <= 450:
             mob.run()
 
     screen.fill((69, 68, 89))
@@ -66,10 +92,15 @@ while running:
     text = font.render(f'{player.health}', True, BLACK)
     screen.blit(text, (145, 5))
 
+    if ABILITY:
+        font = pygame.font.Font(None, 24)
+        text = font.render(f'THE WORLD Ready', True, BLACK)
+        screen.blit(text, (800, 5))
+
     hero_sprite.draw(screen)
     mobs_sprite.draw(screen)
 
-    print(clock.get_fps())
     clock.tick(FPS)
     pygame.display.flip()
+
 pygame.quit()
