c026c349e1a1ace586fc1d3d055c680ea18786ef
cliss <95913122+cliss1@users.noreply.github.com>
2021-12-24 02:25:44 +0300

    не нужна

--- map_lvl.py
+++
@@ -1,9 +0,0 @@
-from settings import *
-
-
-class Map(name):
-    def __init__(self, Name):
-        super(self, Name).__init__()
-        self.map_load = open(Name, encoding='utf-8')
-        self.line = [len(line) for line in self.map_load.read().split('\n')[0]]
-        print(self.line)
