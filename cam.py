d9a5a15105ddc9fedce43130efe2b7471ae24125
cliss <95913122+cliss1@users.noreply.github.com>
2021-12-25 00:13:39 +0300

    хз

--- cam.py
+++ cam.py
@@ -2,17 +2,17 @@ from settings import *
 
 
 class Camera:
-    # зададим начальный сдвиг камеры
     def __init__(self):
         self.dx = 0
         self.dy = 0
 
-    # сдвинуть объект obj на смещение камеры
     def apply(self, obj):
         obj.rect.x += self.dx
         obj.rect.y += self.dy
 
-    # позиционировать камеру на объекте target
     def update(self, target):
         self.dx = -(target.rect.x + target.rect.w // 2 - WIDTH // 2)
         self.dy = -(target.rect.y + target.rect.h // 2 - HEIGHT // 2)
+
+
+camera = Camera()
