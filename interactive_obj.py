1cd260b8b4eb4fa0289a3fb78c30a148b8a053cf
cliss <95913122+cliss1@users.noreply.github.com>
2021-12-25 23:08:01 +0300

    edit name

--- interactive_obj.py
+++ interactive_obj.py
@@ -1,8 +1,9 @@
 import pygame
 from pygame.sprite import AbstractGroup
+from hero_and_mobs import player
 
 
-class obj(pygame.sprite.Sprite):
+class Coin(pygame.sprite.Sprite):
     def __init__(self, *groups: AbstractGroup):
         super().__init__(*groups)
         self.fl = False
@@ -12,17 +13,18 @@ class obj(pygame.sprite.Sprite):
         self.rect = self.image.get_rect()
         self.rect.x, self.rect.y = 100, 100
 
-    def Coin(self):
-        pass
-        # if 4 >= self.frame >= 1 and not self.fl:
-        #     self.frame += 1
-        #     if self.frame == 4:
-        #         self.fl = True
-        # elif self.fl:
-        #     self.fl -= 1
-        #     if self.frame == 1:
-        #         self.fl = False
+    def play_animation_coin(self):
+        self.frame = 1
+        self.fl = False
+        if 4 >= self.frame >= 1 and not self.fl:
+            self.frame += 1
+            if self.frame == 4:
+                self.fl = True
+        elif self.fl:
+            self.fl -= 1
+            if self.frame == 1:
+                self.fl = False
 
 
 coin_sprite = pygame.sprite.Group()
-coi = obj(coin_sprite)
+coi = Coin(coin_sprite)
