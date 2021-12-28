45b0c51020cb97788376f02e4a2af20a7b5a9801
cliss <95913122+cliss1@users.noreply.github.com>
2021-12-28 16:15:46 +0300

    Update Sound_effect.py

--- Sound_effect.py
+++ Sound_effect.py
@@ -4,8 +4,8 @@ import pygame
 class Sound(pygame.sprite.Sprite):
     def __init__(self):
         super().__init__()
-        self.die_hero = pygame.mixer.Sound('sounds/health/player/dead.wav')
-        self.hit_hero = pygame.mixer.Sound('sounds/health/player/hit.wav')
+        self.die_hero = pygame.mixer.Sound('sounds/dead.wav')
+        self.hit_hero = pygame.mixer.Sound('sounds/hit.wav')
 
     def play_hit(self):
         pass
