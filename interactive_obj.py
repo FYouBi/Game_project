bab03e1715a6d179404b9935d9c5b5ce254453a0
cliss <95913122+cliss1@users.noreply.github.com>
2022-02-14 21:50:54 +0300

    изменен класс отвечающий за частицы

--- interactive_obj.py
+++ interactive_obj.py
@@ -1,3 +1,5 @@
+import random
+
 from settings import *
 import pygame
 from pygame.sprite import AbstractGroup
@@ -6,7 +8,15 @@ kill = True
 
 
 def spawn_coin(pos):
-    Coin(pos, coin_sprite)
+    Coin(pos)
+
+
+def create_particles(position, particle_count, x, y, gravity):
+    # количество создаваемых частиц
+    # возможные скорости
+    velocity = range(int(x[0]), int(x[1])), range(int(y[0]), int(y[1]))
+    for _ in range(particle_count):
+        Particle(position, random.choice(velocity[0]), random.choice(velocity[1]), gravity)
 
 
 class Coin(pygame.sprite.Sprite):
@@ -15,11 +25,12 @@ class Coin(pygame.sprite.Sprite):
         self.fl = False
         self.frame = 1
         self.count = 0
+        self.coin = True
         self.image = pygame.image.load(f'images/coin{self.frame}.png')
-        self.image = pygame.transform.scale(self.image, (32, 32))
         self.coin_mask = pygame.mask.from_surface(self.image)
         self.rect = self.image.get_rect()
-        self.rect.centerx, self.rect.centery = pos
+        self.rect.centerx, self.rect.centery = pos[0], pos[1] + 20
+        coin_sprite.add(self)
 
     def update(self):
         self.frame += 1 if self.frame < 6 else -5
@@ -36,15 +47,6 @@ class Ground(pygame.sprite.Sprite):
         self.rect.x, self.rect.y = pos[0], pos[1]
 
 
-class Dirt(pygame.sprite.Sprite):
-    def __init__(self, pos, screen, *groups: AbstractGroup):
-        super().__init__(*groups)
-        self.image = pygame.image.load(f'images/gryaz-export.png').convert_alpha(screen)
-        self.rect = self.image.get_rect()
-        self.mask = pygame.mask.from_surface(self.image)
-        self.rect.centerx, self.rect.centery = pos
-
-
 class AidKit(pygame.sprite.Sprite):
     def __init__(self, pos, *groups: AbstractGroup):
         super().__init__(*groups)
@@ -54,8 +56,40 @@ class AidKit(pygame.sprite.Sprite):
         self.rect.x, self.rect.y = pos
 
 
-dirt = pygame.sprite.Group()
+class Particle(pygame.sprite.Sprite):
+    # сгенерируем частицы разного размера
+    fire = []
+    for scale in (3, 5):
+        fire.append(pygame.transform.scale(pygame.image.load('images/box.png'), (scale, scale)))
+
+    def __init__(self, pos, dx, dy, gravity):
+        super().__init__(coin_sprite)
+        self.image = random.choice(self.fire)
+        self.rect = self.image.get_rect()
+        self.coin = False
+        # у каждой частицы своя скорость — это вектор
+        self.velocity = [dx, dy]
+        # и свои координаты
+        self.rect.x, self.rect.y = pos
+
+        # гравитация будет одинаковой (значение константы)
+        self.gravity = gravity
+        particle_sprite.add(self)
+
+    def update(self):
+        # применяем гравитационный эффект:
+        # движение с ускорением под действием гравитации
+        self.velocity[1] += self.gravity
+        # перемещаем частицу
+        self.rect.x += self.velocity[0]
+        self.rect.y += self.velocity[1]
+        # убиваем, если частица ушла за экран
+        if not self.rect.colliderect((0, 0, WIDTH, HEIGHT)):
+            self.kill()
+
+
 ground_first = pygame.sprite.Group()
+particle_sprite = pygame.sprite.Group()
 coin_sprite = pygame.sprite.Group()
 ground_second = pygame.sprite.Group()
 aid_kit = pygame.sprite.Group()
